2f55881541e45ddc7aa0cb624257e1584453eed7
prateekb1912 <prateekb1912@gmail.com>
2022-08-05 14:24:15 +0530

    Add methods to encode str, int and list

--- bencoding.py
+++ bencoding.py
@@ -22,3 +22,35 @@ class Encoder:
         """
 
         return self.encode_next(self._data)
+    
+    def encode_next(self, data):
+        if type(data) == str:
+            return self._encode_string(data)
+        elif type(data) == int:
+            return self._encode_int(data)
+        elif type(data) == list:
+            return self._encode_list(data)
+    
+    def _encode_string(self, data):
+        return str.encode(str(len(data))+':' + data)
+
+    def _encode_int(self, data):
+        return str.encode('i'+str(data)+'e')
+
+    def _encode_list(self, data):
+        
+        res = 'l'
+
+        for elem in data:
+            if type(elem) == str:
+                res += f'{len(elem)}:{elem}'
+            elif type(elem) == int:
+                res += f'i{elem}e'
+        
+        res += 'e'
+
+        return str.encode(res)
+
+encoder = Encoder(['spam', 'eggs', 123])
+
+print(encoder.encode())
