d0038c0bf9c99aec1454fa97de8eb1ea74751a32
prateekb1912 <prateekb1912@gmail.com>
2022-08-25 14:14:40 +0530

    Refactor changes

--- tests/test_bencoding.py
+++ tests/test_bencoding.py
@@ -1,5 +1,11 @@
-import unittest
 from collections import OrderedDict
 
-from bencoding import Decoder, Encoder
+from main.bencoding import Decoder, Encoder
 
+class DecodingTests():
+
+    def test_peek_is_idempotent(self):
+        decoder = Decoder(b'12')
+        
+        self.assertEqual(b'1', decoder._peek())
+        self.assertEqual(b'2', decoder._peek())
